4b17ac9f86c8fae3f0c474504f9a0a6549c066e0
Liel Feldman <Lielfe@payoneer.com>
2021-04-19 20:27:37 +0300

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -5,14 +5,12 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/<name-of-app>'));
-app.get('*', (req, res) => {
-  res.sendFile(`./front-end/dist/index.html`); // load the single view file (angular will handle the page changes on the front-end)
-});
-// app.get('/*', function(req,res) {
+app.use(express.static(__dirname + '/dist/flute'));
 
-// res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
-// });
+app.get('/*', function(req,res) {
+
+res.sendFile(path.join(__dirname+'/dist/flute/index.html'));
+});
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
